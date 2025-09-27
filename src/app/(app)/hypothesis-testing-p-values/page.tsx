d4a7256b9313b5aabb422c753a425beac93f2494
Prince Khurana <princekhuranadu@gmail.com>
2025-09-27 08:58:37 +0000

    lets fn root cause could it be because of the seo things sitemao things

--- src/app/(app)/hypothesis-testing-p-values/page.tsx
+++ src/app/(app)/hypothesis-testing-p-values/page.tsx
@@ -77,7 +77,7 @@ export default function HypothesisTestingPage() {
                                  </li>
                             ))}
                         </ul>
-                    </CardContent>
+                    </Content>
                 </Card>
             </div>
         </section>
