5a8f7c821c1114857aacbaf2da088a7555adc970
Prince Khurana <princekhuranadu@gmail.com>
2025-10-01 18:26:52 +0000

    fixing json

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,3 @@
-// Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom'
+// This file extends Jest with custom matchers from @testing-library/jest-dom.
+// This gives you access to matchers like .toBeInTheDocument(), .toHaveClass(), etc.
+import '@testing-library/jest-dom';
