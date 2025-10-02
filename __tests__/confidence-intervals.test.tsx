2c8fd4420b4461eb5b39acac1b285eed6ee4d4ed
Prince Khurana <princekhuranadu@gmail.com>
2025-10-02 19:41:06 +0000

    add machine subtopic

--- __tests__/confidence-intervals.test.tsx
+++ __tests__/confidence-intervals.test.tsx
@@ -1,4 +1,3 @@
-
 import { render, screen, fireEvent } from '@testing-library/react';
 import ConfidenceIntervalsPage from '@/app/(app)/topics/confidence-intervals/page';
 
--- __tests__/logo.test.tsx
+++ __tests__/logo.test.tsx
@@ -1,4 +1,3 @@
-
 import { render, screen } from '@testing-library/react';
 import { Logo } from '@/components/app/logo';
 
