1233ddca78a34d1e8379d288b4dfa850cf9ebcdb
Prince Khurana <princekhuranadu@gmail.com>
2025-10-03 23:51:54 +0000

    2025-10-03T23:43:13Z [web] <Firebase Studio> Starting preview... 2025-10

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: false,
@@ -35,7 +33,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -44,4 +42,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
