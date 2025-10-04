ac5e8c61f99d8a7c6c44652ec5a8cb0d5763e809
Prince Khurana <princekhuranadu@gmail.com>
2025-10-04 01:40:11 +0000

    [{ 	"resource": "/home/user/studio/__tests__/confidence-intervals.test.t

--- __tests__/confidence-intervals.test.tsx
+++ __tests__/confidence-intervals.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/dom';
 import ConfidenceIntervalsPage from '@/app/(app)/topics/confidence-intervals/page';
 
 describe('ConfidenceIntervalsPage', () => {
