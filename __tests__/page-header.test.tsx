29236c829b2c019f292ab61488ce1e4ac9d2e0e2
Prince Khurana <princekhuranadu@gmail.com>
2025-10-04 02:00:25 +0000

    File changes

--- __tests__/page-header.test.tsx
+++ __tests__/page-header.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
 import { PageHeader } from '@/components/app/page-header';
 
 describe('PageHeader', () => {
