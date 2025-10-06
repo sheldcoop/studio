bacd2b4c91e44127b13c4768272be0c8fd6111e1
Prince Khurana <princekhuranadu@gmail.com>
2025-10-06 22:15:23 +0000

    Of course. You're getting these errors due to a combination of incorrect

--- src/app/(app)/quantlab/anova/component.tsx
+++ src/app/(app)/quantlab/anova/component.tsx
@@ -14,14 +14,14 @@ import { Skeleton } from '@/components/ui/skeleton';
 const RechartsAreaChart = dynamic(() => import('recharts').then(mod => mod.AreaChart), { ssr: false, loading: () => <Skeleton className="h-full w-full" /> });
 const RechartsBarChart = dynamic(() => import('recharts').then(mod => mod.BarChart), { ssr: false, loading: () => <Skeleton className="h-full w-full" /> });
 const RechartsLineChart = dynamic(() => import('recharts').then(mod => mod.LineChart), { ssr: false, loading: () => <Skeleton className="h-full w-full" /> });
-const Area = dynamic(() => import('recharts').then(mod => mod.Area as any), { ssr: false });
-const Bar = dynamic(() => import('recharts').then(mod => mod.Bar as any), { ssr: false });
-const Line = dynamic(() => import('recharts').then(mod => mod.Line as any), { ssr: false });
-const CartesianGrid = dynamic(() => import('recharts').then(mod => mod.CartesianGrid as any), { ssr: false });
-const XAxis = dynamic(() => import('recharts').then(mod => mod.XAxis as any), { ssr: false });
-const YAxis = dynamic(() => import('recharts').then(mod => mod.YAxis as any), { ssr: false });
-const Tooltip = dynamic(() => import('recharts').then(mod => mod.Tooltip as any), { ssr: false });
-const Cell = dynamic(() => import('recharts').then(mod => mod.Cell as any), { ssr: false });
+const Area = dynamic(() => import('recharts').then(mod => mod.Area), { ssr: false });
+const Bar = dynamic(() => import('recharts').then(mod => mod.Bar), { ssr: false });
+const Line = dynamic(() => import('recharts').then(mod => mod.Line), { ssr: false });
+const CartesianGrid = dynamic(() => import('recharts').then(mod => mod.CartesianGrid), { ssr: false });
+const XAxis = dynamic(() => import('recharts').then(mod => mod.XAxis), { ssr: false });
+const YAxis = dynamic(() => import('recharts').then(mod => mod.YAxis), { ssr: false });
+const Tooltip = dynamic(() => import('recharts').then(mod => mod.Tooltip), { ssr: false });
+const Cell = dynamic(() => import('recharts').then(mod => mod.Cell), { ssr: false });
 
 
 // Helper function to generate normally distributed data
@@ -320,5 +320,3 @@ export default function AnovaPage() {
     </>
   );
 }
-
-    
