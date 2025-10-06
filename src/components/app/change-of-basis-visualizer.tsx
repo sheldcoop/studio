999fc9413b3b27cc28f1e32b2f6c6b434dc9151b
Prince Khurana <princekhuranadu@gmail.com>
2025-10-06 19:48:13 +0000

    refactor

--- src/components/app/change-of-basis-visualizer.tsx
+++ src/components/app/change-of-basis-visualizer.tsx
@@ -9,7 +9,7 @@ import { Slider } from '@/components/ui/slider';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { drawGrid, drawVector, easeInOutCubic, screenToWorld, drawPoint } from '@/lib/p5';
+import { drawGrid, drawVector, easeInOutCubic, screenToWorld, drawPoint } from '@/lib/p5/index.ts';
 import { calculateEigen } from '@/lib/math/linear-algebra';
 
 const ChangeOfBasisVisualizer = () => {
