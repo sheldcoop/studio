e3d0fb7e5daad92228249127cd5d3592d85e5d3c
Prince Khurana <princekhuranadu@gmail.com>
2025-10-07 12:05:43 +0000

    fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           fontHeadline.variable
         )}
       >
-        {/* @ts-ignore */}
+      
         <py-config>
           packages = ["numpy", "scipy", "matplotlib", "scikit-image"]
           terminal = false
