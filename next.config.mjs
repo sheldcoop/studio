c3d4cdc8d296da2177159c9f33e511fa35824066
Prince Khurana <princekhuranadu@gmail.com>
2025-10-09 00:17:21 +0000

    ff

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,11 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
   
-  experimental: {
-    allowedDevOrigins: [
-      '6000-firebase-quantprep3-1759145568602.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev',
-      'https://6000-firebase-quantprep3-1759145568602.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev'
-    ]
-  }
+  // Top-level config for Next.js 15+
+  allowedDevOrigins: [
+    '6000-firebase-quantprep3-1759145568602.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev',
+    'https://6000-firebase-quantprep3-1759145568602.cluster-fnjdffmttjhy2qqdugh3yehhs2.cloudworkstations.dev'
+  ]
 };
 
 export default nextConfig;
