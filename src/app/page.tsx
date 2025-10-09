e11e9dbce063f5051e8fc652bd60c1cb9ebdab3d
Prince Khurana <princekhuranadu@gmail.com>
2025-10-09 10:02:12 +0000

    package

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+'use client';
+import HomePageClient from '@/components/app/home-page-client';
+
+
+export default function RootPage() {
+
+  return (
+    <div className="flex-1">
+        <HomePageClient />
+    </div>
+  );
+}
