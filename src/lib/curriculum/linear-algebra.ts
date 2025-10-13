358298a8a563f440df5096003723d8014703e125
Prince Khurana <princekhuranadu@gmail.com>
2025-10-13 18:16:33 +0000

    its showing page not found 404 please create a new lib.ts this is experi

--- src/lib/curriculum/linear-algebra.ts
+++ src/lib/curriculum/linear-algebra.ts
@@ -2,24 +2,10 @@
 import { type Topic } from './types';
 import { linearAlgebraPart1 } from './linear-algebra-part-1';
 import { linearAlgebraPart2 } from './linear-algebra-part-2';
-import { createTopic } from './utils';
-
-const animationTopics: Topic[] = [
-    createTopic({
-        parent: 'la-anim-module-1',
-        id: 'visualizing-determinant',
-        title: 'Visualizing the Determinant',
-        icon: 'BoxSelect',
-        description: 'An interactive visualization of how a matrix transforms space and how the determinant measures this change.',
-        category: 'sub-topic',
-        duration: 10,
-    }),
-]
 
 const rawTopics: Topic[] = [
     ...linearAlgebraPart1,
     ...linearAlgebraPart2,
-    ...animationTopics,
 ];
 
 // Post-process to fix hrefs
