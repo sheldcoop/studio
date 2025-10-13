b837c0398f5d24f39b57190610b0a16b46708b46
Prince Khurana <princekhuranadu@gmail.com>
2025-10-13 09:35:26 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   experimental: {},
 };
 
